2a3e1fb18a753424b6d2f702226e4d5663f67ef3
shayonj <its@shayon.me>
2013-03-30 01:32:32 +0530

    commenting stuff. half done. need to hit bed now before i collapse on this machine

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,8 +10,11 @@ class SearchesController < ApplicationController
   end
   
   def create
+    # Instantiate and query through twitter results against the hashtags. .results is a twitter api parameter, which helps returning tweet info against the hashtag search quesry.
     twitter_search = TwitterSearch.new
     @twitter_results = twitter_search.query(:tag => params[:search]).results
+    
+    # Instantiate and query through instagram results against the hashtags
     instagram_search = InstagramSearch.new
     @instagram_results = instagram_search.query(:tag => params[:search])
         
