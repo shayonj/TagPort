858e6d08e5a861240a3750ff6a0ea23ae9adf9cd
shayonj <its@shayon.me>
2013-04-02 02:02:18 +0530

    Added twitter scroll fetching next set of tweets with max id. Instagram remaining, also need to alternate the views for both

--- app/classes/twitter_search.rb
+++ app/classes/twitter_search.rb
@@ -1,9 +1,8 @@
 class TwitterSearch 
-  attr_reader :client, :tag, :count, :lang, :result_type
+  attr_reader :client, :tag, :count, :lang, :result_type, :max_id
   
   def initialize
     @client = Twitter::Client.new(
-      :endpoint => "http://api.twitter.com",
       :consumer_key => ENV["TWITTER_CONSUMER_KEY"], 
       :consumer_secret => ENV["TWITTER_CONSUMER_SECRET"],
       :oauth_token => ENV["TWITTER_OAUTH_TOKEN"],
@@ -13,13 +12,15 @@ class TwitterSearch
   end
   
   
-  def query(args)
+  def first_query(args)
     @tag = args[:tag]
     @count = args[:count] || 10
     @lang = args[:lang] || "en"
     @result_type = args[:result_type] || "recent"
+    @max_id = args[:max_id] || 0
     
-    client.search("#".concat(tag), :count => count, :lang => lang, :result_type => result_type)
+    client.search("#".concat(tag), :count => count, :lang => lang, :result_type => result_type, :max_id => max_id)
   end
+  
 
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
-  
+
   def new
   end
 
@@ -8,16 +8,24 @@ class SearchesController < ApplicationController
 
   def show
   end
-  
+
   def create
-    # Instantiate and query through twitter results against the hashtags. .results is a twitter api parameter, which helps returning tweet info against the hashtag search quesry.
+
     twitter_search = TwitterSearch.new
-    @twitter_results = twitter_search.query(:tag => params[:search]).results
-    
-    # Instantiate and query through instagram results against the hashtags
     instagram_search = InstagramSearch.new
-    @instagram_results = instagram_search.query(:tag => params[:search])
-        
+      
+    if params[:search].nil? == true
+      @twitter_results = twitter_search.first_query(:tag => params[:tag], :count =>20, :max_id => params[:twitter_last_id])
+      @instagram_results = instagram_search.query(:tag => params[:tag], :count => 20)
+    else
+      @twitter_results = twitter_search.first_query(:tag => params[:search], :count =>20)
+      @instagram_results = instagram_search.query(:tag => params[:search], :count => 20)
+    end
+    
+     #Instantiate and query through instagram results against the hashtags
+     
+       
+
     respond_to do |format|
       format.js
     end
