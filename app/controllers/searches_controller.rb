57994f77ec4fe0880f9680041528b6c67c21cdef
shayonj <its@shayon.me>
2013-04-03 11:03:10 +0530

    tag never returns empty now.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,4 @@
 class SearchesController < ApplicationController
-
   def new
   end
 
@@ -12,17 +11,24 @@ class SearchesController < ApplicationController
   def create
     twitter_search = TwitterSearch.new
     instagram_search = InstagramSearch.new
+    
+    # Making sure @tag is never return empty
+    if params[:tag].nil? == true
+      @tag = params[:search]
+    else
+      @tag = params[:tag]
+    end
 
     if params[:tag].nil? == false
-      @twitter_results = twitter_search.first_query(:tag => params[:tag], :count =>20, :max_id => params[:twitter_last_id])
-      @instagram_results = instagram_search.query(:tag => params[:tag], :count => 20, :max_tag_id => params[:instagram_max_tag_id])
+      @twitter_results = twitter_search.first_query(:tag => params[:tag], :count =>28, :max_id => params[:twitter_last_id])
+      @instagram_results = instagram_search.query(:tag => params[:tag], :count => 28, :max_tag_id => params[:instagram_max_tag_id])
     else
-      @twitter_results = twitter_search.first_query(:tag => params[:search], :count =>20)
-      @instagram_results = instagram_search.query(:tag => params[:search], :count => 20)
+      @twitter_results = twitter_search.first_query(:tag => params[:search], :count =>28)
+      @instagram_results = instagram_search.query(:tag => params[:search], :count => 28)
     end
 
     respond_to do |format|
-      format.js
+      format.js {render :content_type => "text/javascript"}
     end
   end
 end
