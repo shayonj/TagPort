0fab4c9704b682de9e35f4c5d2e538b5145b1335
Shayon Mukherjee <its@shayon.me>
2013-04-12 23:05:07 +0530

    Added a custom loader.

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,9 +1,13 @@
 module SearchesHelper
   
-  # Converting links in text as hyperlins and hyperlinking the twitter mentions accordingly. A little messy but works neat ;)
+  # Converting links in text as hyperlins and hyperlinking the twitter/instagram mentions accordingly. A little messy but works neat ;)
 
-  def cust_link(text)
+  def cust_link_twitter(text)
     text.gsub(/((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/, %Q{<a href="\\1", class="custom", target="_blank">\\1</a>}).gsub(/@(\w+)/, %Q{<a href="http://twitter.com/\\1", class="custom", target="_blank">@\\1</a>}).html_safe
+  end
+  
+  def cust_link_instagram(text)
+    text.gsub(/@(\w+)/, %Q{<a href="http://instagram.com/\\1", target="_blank">@\\1</a>}).html_safe
   end
 
 end
